3e4ff2806728be4a7729cb3faa8175999234d06d
dhananjaychoudhari <76993420+dhananjaychoudhari@users.noreply.github.com>
2021-08-15 15:32:58 +0530

    Add files via upload

--- src/components/App.js
+++ src/components/App.js
@@ -4,12 +4,19 @@ import '../styles/App.css';
 class App extends Component {
     constructor(props) {
 		super(props);
+		this.state = {
+			pHidden : false,
+		};
 	};
-
-    render() {
+	render() {
     	return(
     		<div id="main">
-				{ /* Do not remove this main div!! */ }
+				<button id="click" 
+				onClick={() =>{this.setState({pHidden : true});}
+				}>show para</button>
+				{this.state.pHidden ? (<p id="para">Hello, I've learnt to
+				 use the full-stack evaluation tool. This makes me so happy</p>):null}
+				
     		</div>
     	);
     }
